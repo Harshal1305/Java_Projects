24f2d791f59ccb983809f4ee404e28dfe4398511
Harshal Waychal <waychalharshal@gmail.com>
2025-06-25 20:30:19 +0530

    Second commit

--- Demo.java
+++ Demo.java
@@ -3,5 +3,7 @@ public class Demo {
         System.out.println("Hi This is Harshal");
 
         System.out.println("This is Yash");
+
+        System.out.println("I am good to start java from tomorrow.......Hurray!!!");
     }
 }
