0f38519f4611435e3df51463a5ff71e23aa7a27a
Harshal Waychal <waychalharshal@gmail.com>
2025-06-27 16:07:06 +0530

    last final

--- VariableExample.java
+++ VariableExample.java
@@ -7,7 +7,7 @@ public class VariableExample {
         double area;
         double pi = 3.14;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the radius of the circle: ");
+        System.out.println("Enter the radius of the circle: ");
         r = sc.nextDouble();
         area = pi * r * r;  
         System.out.println("Area of the circle: " + area);   
