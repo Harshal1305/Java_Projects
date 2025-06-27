8aa6a5c745d3b604173ac180220c6503ce017e81
Harshal Waychal <waychalharshal@gmail.com>
2025-06-27 16:03:41 +0530

    Final PS1_lec2

--- VariableExample.java
+++ VariableExample.java
@@ -10,7 +10,22 @@ public class VariableExample {
         System.out.print("Enter the radius of the circle: ");
         r = sc.nextDouble();
         area = pi * r * r;  
-        System.out.println("Area of the circle: " + area);
-        sc.close();     
+        System.out.println("Area of the circle: " + area);   
+        
+        //Printing table of a number
+        System.out.print("Enter a number to print its table: ");
+        int num = sc.nextInt(); 
+        System.out.println("Table of " + num + ":");
+        System.out.println(num + " * 1" + " = " + (num * 1));
+        System.out.println(num + " * 2" + " = " + (num * 2));
+        System.out.println(num + " * 3" + " = " + (num * 3));        
+        System.out.println(num + " * 4" + " = " + (num * 4));
+        System.out.println(num + " * 5" + " = " + (num * 5));
+        System.out.println(num + " * 6" + " = " + (num * 6));
+        System.out.println(num + " * 7" + " = " + (num * 7));
+        System.out.println(num + " * 8" + " = " + (num * 8));
+        System.out.println(num + " * 9" + " = " + (num * 9));
+        System.out.println(num + " * 10" + " = " + (num * 10));
+        sc.close();
     }
 }
