36c9e1019d2086041b9607e78c5c446bdb00837d
Harshal Waychal <waychalharshal@gmail.com>
2025-07-01 21:10:55 +0530

    Patterns with problem statements lec 5

--- Patterns.java
+++ Patterns.java
@@ -44,6 +44,94 @@ public class Patterns {
             System.out.println();
         } */ 
 
+        /* //Printing inverted half pyramid
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=(row-i); j++){
+                System.out.print(" ");               
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+             
+        System.out.println();
+        } */
 
+        /* //Printing numbers in half pyramid
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        } */
+
+        /* //Printing numbers in half pyramid inverted
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=(row-i+1); j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        } */
+
+        /* //Printing Floyd's triangle
+        int num =1;
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i;j++){
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        } */
+
+        /* //Printing 0-1 triangle
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        } */
+
+        /* //Printing solid rhombus
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=(row-i); j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=row; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } */
+
+        /* //Printing number pyramid
+        for(int i=1; i<=row; i++){
+            for(int j=(row/2); j>=i; j--){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print(i);
+            }
+            System.out.println();
+        } */
+
+        /* //Printing palindromic number pyramid
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=(row-i); j++){
+                System.out.print(" ");
+            }
+            for(int k=i; k<=i; k++){
+                System.out.print(k);
+            }
+            for(int l=(i-1); l>=1; l--){
+                System.out.print(l);
+            }
+            for(int m=1;m<i; m++){
+                System.out.print(m+1);
+            }
+            System.out.println();
+        } */
+        
      }
 }
