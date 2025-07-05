6291e7183e20f7f28fd15403751940dc14531265
Harshal Waychal <waychalharshal@gmail.com>
2025-07-05 20:29:25 +0530

    Functions

---
+++ Function.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Function {
+    
+    public static void main(String[] args) {
+        //Printing sum by function
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        primeNumber(a);
+        /* int b = sc.nextInt();
+        int sum = add(a, b);
+        System.out.println("Sum is: "+ sum); */
+    }
+
+    /* public static int add(int a, int b){
+        int result = a + b;
+        return result;
+    } */
+
+    /* public static void primeNumber(int a) {
+        for(int i=2; i<=(a/2); i++){
+            if(a % i == 0){
+                System.out.println("Not a prime number");
+            }   
+        }
+        System.out.println("Prime number");
+    } */
+
+
+}
